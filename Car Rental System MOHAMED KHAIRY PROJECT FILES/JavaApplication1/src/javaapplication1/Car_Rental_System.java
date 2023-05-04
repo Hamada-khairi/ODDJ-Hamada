8f7b70daefa6c28c599cd3473c6388d6e01a4edd
Hamada Khairi <tp066168@mail.apu.edu.my>
2023-05-04 16:16:51 +0800

    Hamada commit

---
+++ Car Rental System MOHAMED KHAIRY PROJECT FILES/JavaApplication1/src/javaapplication1/Car_Rental_System.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication1;
+
+/**
+ *
+ * @author Wajahat Qazi
+ */
+public class Car_Rental_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      Welcome abc=new Welcome();
+       abc.setVisible(true);
+    }
+    
+}
